4da380b22a3cdd1d7cc740db76dd8af04283c0d0
Rick Companje <rick@companje.nl>
2023-03-10 22:45:46 +0100

    Update 4.maisflexis-export-to-json.sh

--- 4.maisflexis-export-to-json.sh
+++ 4.maisflexis-export-to-json.sh
@@ -1,5 +1,5 @@
 # converteer het Export XML bestand van MaisFlexis naar JSON
-# dit script wordt op een webserver uitgevoerd
+# de conversie wordt via curl remote uitgevoerd
 
 URL=http://mfxml2json.0x03.nl:8888/run
 INPUT_FILE=data/maisflexis/nieuw-A0003_14_158_flexis.txt
