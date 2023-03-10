58a55a1e55fc4b93a7af2e3a362db348896993a7
Rick Companje <rick@companje.nl>
2023-03-10 22:48:36 +0100

    Update 4.mais-export-to-json.sh

--- 4.mais-export-to-json.sh
+++ 4.mais-export-to-json.sh
@@ -2,8 +2,8 @@
 # de conversie wordt via curl remote uitgevoerd
 
 URL=http://mfxml2json.0x03.nl:8888/run
-INPUT_FILE=data/maisflexis/nieuw-A0003_14_158_flexis.txt
-OUTPUT_FILE=data/maisflexis/nieuw-A0003_14_158_flexis.json
+INPUT_FILE=data/mais/nieuw-A0003_14_158_flexis.txt
+OUTPUT_FILE=data/mais/nieuw-A0003_14_158_flexis.json
 curl -J -X POST -F "file=@$INPUT_FILE" $URL > $OUTPUT_FILE
 
 echo $OUTPUT_FILE
