da80bff216d31b6747db5efc049e47e454b92409
Rick Companje <rick@companje.nl>
2023-03-12 17:11:57 +0100

    update

--- 4.mais-export-to-json.sh
+++
@@ -1,9 +0,0 @@
-# converteer het Export XML bestand van Mais naar JSON
-# de conversie wordt via curl remote uitgevoerd
-
-URL=http://mfxml2json.0x03.nl:8888/run
-INPUT_FILE=data/mais/nieuw-A0003_14_158_flexis.txt
-OUTPUT_FILE=data/mais/nieuw-A0003_14_158_flexis.json
-curl -J -X POST -F "file=@$INPUT_FILE" $URL > $OUTPUT_FILE
-
-echo $OUTPUT_FILE
---
+++ mapping-utils.py
@@ -0,0 +1,91 @@
+from liquid import Liquid
+import json,csv,hashlib,re,sys
+from datetime import datetime
+from rdflib import Graph, Literal
+
+def data_mapping(input_filename, header_filename, template_filename, output_filename, limit=0):
+  items = load_items(input_filename)
+
+  if limit>0:
+    items = items[:limit]
+
+  filters = {
+      "make_safe_literal": make_safe_literal,
+      "is_valid_isodate": is_valid_isodate,
+      "md5": md5
+  }
+
+  ttl = open(header_filename).read()
+  ttl += apply_liquid_templates(items=items, template_filename=template_filename, filters=filters)
+
+  g = Graph()
+  g.parse(data=ttl)
+  g.remove((None, None, Literal(""))) #remove triples with empty literals
+  with open(output_filename,"w") as f:
+    print(g.serialize(),file=f)
+
+def make_safe_literal(s): # replace single " quote by double "" and add quotes
+  s = s.strip()
+  s = re.sub(r"\n", "\\\\n", s)
+  s = re.sub(r"\"", "\\\"", s)
+  return s
+
+def make_safe_URI_part(s):
+  # spreadsheet: [–’&|,\.() ""$/':;]"; "-") ;"-+";"-"); "[.-]$"; ""))
+  s = re.sub(r"[–’+?&=|,\.() \"$/']", "-", s) # replace different characters by a dash
+  s = re.sub(r"-+", "-", s) # replace multiple dashes by 1 dash
+  s = re.sub(r"[^a-zA-Z0-9\-]", "", s) # strip anything else now that is not a alpha or numeric character or a dash
+  s = re.sub(r"^-|-$", "", s) # prevent starting or ending with . or -
+  if len(s)==0:
+    #raise ValueError("makeSafeURIPart results in empty string")
+    # log.warning("makeSafeURIPart results in empty string")
+    # fix this by replacing by 'x' for example
+    s="x"
+  return s.lower()
+
+def make_safe_key_name(s): # for Liquid templates
+  return s.replace(' ', '_').replace('?','')
+
+def make_safe_keys(item):
+  return { make_safe_key_name(x): v for x, v in item.items() } # renames keynames to use in template
+
+def replace_keys_by_values(s,item,start="{{",end="}}"):
+  for key in item:
+       s = s.replace(f"{start}{key}{end}", str(item[key]))
+  return s
+
+def apply_liquid_template(template_filename, item, filters={}):
+    return Liquid(template_filename, liquid_from_file=True, filters=filters).render(**item)
+
+def load_json_items(filename):
+    return json.load(open(filename, encoding='utf-8-sig'))
+
+def load_csv_items(filename, delimiter=","):
+    return list(csv.DictReader(open(filename, encoding='utf-8-sig'), delimiter=delimiter))
+
+def load_items(filename):
+  if filename.endswith(".csv"):
+    return load_csv_items(filename)
+  elif filename.endswith(".json"):
+    return load_json_items(filename)
+  else:
+    sys.exit(f"unknown file type: {filename}")
+
+def apply_liquid_templates(items, template_filename="templates/Default.ttl", filters={}):
+    return '\n'.join(
+        apply_liquid_template(
+          replace_keys_by_values(template_filename, item), 
+          make_safe_keys(item),
+          filters=filters
+        ) for item in items
+    )
+
+def md5(s):
+    return hashlib.md5(s.encode()).hexdigest()
+
+def is_valid_isodate(dt_str):
+  try:
+    datetime.fromisoformat(dt_str)
+    return True
+  except:
+    return False
